ec9fd141441da71090f949d56e0f56d0b928285c
Albin <imoonfellow@gmail.com>
2024-09-18 00:54:08 +0700

    product key fixed

--- main/views.py
+++ main/views.py
@@ -36,9 +36,9 @@ def show_json(request):
     return HttpResponse(serializers.serialize('json', data), content_type='application/json')
 
 def show_xml_by_id(request, id):
-    data = Product.objects.filter(id=id)
+    data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize('xml', data), content_type='application/xml')
 
 def show_json_by_id(request, id):
-    data = Product.objects.filter(id=id)
+    data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize('json', data), content_type='application/json')
