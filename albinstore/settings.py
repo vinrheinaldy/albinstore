e40fdf44b815d65595580af10beb4607f3f5e1e8
Alvin <imoonfellow@gmail.com>
2024-09-24 14:03:59 +0700

    tugas 4 should be done

--- albinstore/settings.py
+++ albinstore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(uu&e9e(04$0588*t90ue4)kp1*v$*4#pb$-x6ct!vdsow(ph*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+PRODUCTION = os.getenv("PRODUCTION", "False")
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","malvin-rheinaldy-albinstoree.pbp.cs.ui.ac.id"]
